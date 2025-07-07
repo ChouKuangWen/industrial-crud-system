1b82e0c9a6c033d17e70a849aba231949a78a6a2
Kuang-Wen,Chou <zx011035@gmail.com>
2025-07-07 20:16:35 +0800

    完成product.html

--- frontend/product.js
+++ frontend/product.js
@@ -6,22 +6,22 @@ function switchMode() {
   if (mode) document.getElementById(mode + "Box").style.display = "block";
 }
 
-async function createProduct_line() {
-  const product_line = {
+async function createProduct() {
+  const product = {
     name: document.getElementById("c_name").value,
-    factory: document.getElementById("c_catagory").value,
-    status: document.getElementById("c_price").value,
-    capacity_per_day: parseInt(document.getElementById("c_capacity_per_day").value) || 0,
+    catagory: document.getElementById("c_catagory").value,
+    price: document.getElementById("c_price").value,
+    spec: document.getElementById("c_spec").value,
   };
   try {
-    await axios.post(api, product_line);
+    await axios.post(api, product);
     alert("✅ 新增成功");
   } catch (err) {
     alert("❌ 錯誤：" + (err.response?.data?.detail || err.message));
   }
 }
 
-async function readProduct_line() {
+async function readProduct() {
   const keyword = document.getElementById("r_keyword").value.toLowerCase();
   const res = await axios.get(api);
   const table = document.getElementById("readTable");
@@ -29,23 +29,23 @@ async function readProduct_line() {
   res.data.forEach(f => {
   if (f.name.toLowerCase().includes(keyword)) {
     table.innerHTML += `<tr>
-      <td>${f.line_id}</td><td>${f.name}</td>
-      <td>${f.factory_id}</td><td>${f.status}</td>
-      <td>${f.ecapacity_per_day || ""}</td>
+      <td>${f.product_id}</td><td>${f.name}</td>
+      <td>${f.category}</td><td>${f.price}</td>
+      <td>${f.spec}</td>
     </tr>`;
     }
   });
 }
 
-async function loadUpdateProduct_line() {
+async function loadUpdateProduct() {
   const id = document.getElementById("u_id").value;
   try {
     const res = await axios.get(`${api}/${id}`);
     const f = res.data;
     document.getElementById("u_name").value = f.name;
-    document.getElementById("u_factory").value = f.factory_id;
-    document.getElementById("u_status").value = f.status;
-    document.getElementById("u_capacity_per_day").value = f.capacity_per_day || "";
+    document.getElementById("u_catagory").value = f.catagory;
+    document.getElementById("u_price").value = f.price;
+    document.getElementById("u_spec").value = f.spec;
     document.getElementById("updateForm").style.display = "block";
   } catch (err) {
     alert("❌ 找不到工廠");
@@ -53,23 +53,23 @@ async function loadUpdateProduct_line() {
   }
 }
 
-async function updateProduct_line() {
+async function updateProduct() {
   const id = document.getElementById("u_id").value;
-  const product_line = {
+  const product = {
     name: document.getElementById("u_name").value,
-    factory_id: document.getElementById("u_factory").value,
-    status: document.getElementById("u_status").value,
-    capacity_per_day: parseInt(document.getElementById("u_capacity_per_day").value) || 0,
+    catagory: document.getElementById("u_catagory").value,
+    price: document.getElementById("u_price").value,
+    spec: document.getElementById("u_spec").value,
   };
   try {
-    await axios.put(`${api}/${id}`, product_line);
+    await axios.put(`${api}/${id}`, product);
     alert("✅ 修改成功");
   } catch (err) {
     alert("❌ 修改失敗：" + err.message);
   }
 }
 
-async function deleteProduct_line() {
+async function deleteProduct() {
   const id = document.getElementById("d_id").value;
   if (!confirm(`確定要刪除工廠 ID ${id} 嗎？`)) return;
     try {
